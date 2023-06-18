8ca551415f9653999c916179d2ec0cee0817fa3b
aidant <15520814+aidant@users.noreply.github.com>
2023-06-18 11:18:09 +1000

    add region

--- packages/infra-sight-aws/test-env.vitest.setup.ts
+++ packages/infra-sight-aws/test-env.vitest.setup.ts
@@ -4,5 +4,5 @@ vi.stubEnv('AWS_BUCKET', 'infra-sight-test-bucket')
 vi.stubEnv('INFRA_SIGHT_API_URL', 'http://localhost:3000/')
 vi.stubEnv(
   'S3_CONFIG',
-  '{"forcePathStyle":true,"credentials":{"accessKeyId":"S3RVER","secretAccessKey":"S3RVER"},"endpoint":"http://localhost:4568"}'
+  '{"forcePathStyle":true,"region":"us-east-1","credentials":{"accessKeyId":"S3RVER","secretAccessKey":"S3RVER"},"endpoint":"http://localhost:4568"}'
 )
